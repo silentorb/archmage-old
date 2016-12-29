fb25ea8395b2cd9636e25e38f048154229983c02
cj <cj@silentorb.com>
2016-12-28 22:06:03 -0700

    Added git and boost filesystem libraries.

--- projects/archmage_test/source/workspace_test.cpp
+++ projects/archmage_test/source/workspace_test.cpp
@@ -1,6 +1,33 @@
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
+#include <boost/filesystem.hpp>
+#include <Repository.h>
+
+using namespace boost;
+using namespace std;
+
+void create_file(const string &name, const string &contents) {
+  ofstream file(name);
+  file << contents;
+}
+
+void create_test_repo(const string &name) {
+  auto path = "temp/repos/" + name;
+  filesystem::create_directories(path);
+  create_file(path + "/README.md", "# " + name + " documentation");
+  create_file(path + "/LICENSE.txt", "This can only be used for awesomeness!");
+  auto git = repoman::Repository(path);
+}
+
+void setup() {
+  filesystem::remove_all("temp");
+  filesystem::create_directories("temp/repos");
+  create_test_repo("audio");
+  create_test_repo("mythic");
+}
 
 TEST(Workspace, general) {
-  ASSERT_TRUE(false);
-  EXPECT_EQ(60, 1);
+  setup();
+
+//  ASSERT_TRUE(false);
+//  EXPECT_EQ(60, 1);
 }
